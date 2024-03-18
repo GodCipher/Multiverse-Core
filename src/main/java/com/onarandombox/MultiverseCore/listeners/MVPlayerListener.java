69554288e8e3df2fc58e6c4d1248683d3db2ed28
Luziferium <luziferisdoof@gmail.com>
2024-03-18 11:23:34 +0100

    Now getting the MVConfig over the plugins instance

--- src/main/java/com/onarandombox/MultiverseCore/listeners/MVPlayerListener.java
+++ src/main/java/com/onarandombox/MultiverseCore/listeners/MVPlayerListener.java
@@ -347,7 +347,7 @@ public class MVPlayerListener implements Listener {
      * @param world The world the player is in.
      */
     public void handleGameModeAndFlight(final Player player, final MultiverseWorld world) {
-        if (!MultiverseCoreConfiguration.getInstance().isHandlingGameModeAndFlight()) {
+        if (!this.plugin.getMVConfig().isHandlingGameModeAndFlight()) {
             Logging.fine("GameMode and Flight handling is disabled in the config." +
                     " Not handling it for player: " + player.getName());
             return;
